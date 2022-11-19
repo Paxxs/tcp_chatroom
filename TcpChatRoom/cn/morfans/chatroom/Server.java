14961ce8d735969c46b7f4b996bc2afde846c26d
Paxxs <10364775+Paxxs@users.noreply.github.com>
2022-11-20 01:40:08 +0800

    服务端启动函数，构造函数，日志输出函数 实现

--- TcpChatRoom/cn/morfans/chatroom/Server.java
+++ TcpChatRoom/cn/morfans/chatroom/Server.java
@@ -3,11 +3,68 @@ package cn.morfans.chatroom;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Server {
     int uniqueID;
+    private final int port;
+    /**
+     * 客户端列表
+     */
+    private final ArrayList<ClientThread> clientList;
+    private boolean keepGoing;
+    private SimpleDateFormat sdf;
+
+    public Server(int port) {
+        this.port = port;
+        this.clientList = new ArrayList<>();
+        this.sdf = new SimpleDateFormat("HH:mm:ss");
+    }
+
+    /**
+     * 启动服务端
+     */
+    public void start() {
+        keepGoing = true;
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while (keepGoing) {
+                log("监听 :" + port + "端口，等待客户端连接...");
+                // 寻找客户端
+                Socket socket = serverSocket.accept();
+                if (!keepGoing) // 中断
+                    break;
+                // 找到了就添加到客户端列表
+                ClientThread thread = new ClientThread(socket);
+                clientList.add(thread);
+                thread.start();
+            }
+            // 结束了就释放资源
+            serverSocket.close();
+            // 释放所有客户端的连接
+            // 使用 foreach loop:for(data_type variable : array_name)
+            for (ClientThread c : clientList) {
+                c.close();
+            }
+        } catch (IOException e) {
+            log("ServerSocket 过程发生错误！");
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 服务端日志输出
+     *
+     * @param msg 日志内容
+     */
+    private void log(String msg) {
+        String nowTime = sdf.format(new Date());
+        System.out.println(nowTime + " " + msg);
+    }
 
     /**
      * 处理客户端的线程
@@ -64,9 +121,18 @@ public class Server {
             while (keepGoing) {
                 try {
                     chatMessage = (ChatMessage) sInput.readObject();
-                } catch (IOException | ClassNotFoundException e) {
-                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    log(userName + " 读入流发生错误:" + e);
+//                    throw new RuntimeException(e);
+                    break;
+                } catch (ClassNotFoundException e2) {
+                    e2.printStackTrace();
+                    break;
                 }
+                /*
+                  解析后的聊天文本
+                 */
+                String message = chatMessage.getMessage();
                 switch (chatMessage.getType()) {
                     case ChatMessage.MESSAGE -> {
                         //TODO　处理发消息
@@ -91,6 +157,7 @@ public class Server {
             try {
                 sOutput.writeObject(msg);
             } catch (IOException e) {
+                log("[客户端线程] 服务器下发消息过程发生错误！");
                 throw new RuntimeException(e);
             }
             return true;
@@ -103,6 +170,7 @@ public class Server {
                 sInput.close();
                 socket.close();
             } catch (IOException e) {
+                log("[客户端线程] 关闭过程发生错误！");
                 throw new RuntimeException(e);
             }
 
